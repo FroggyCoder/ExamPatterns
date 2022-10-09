e5f5b47336005c99489d35ecbd0a6d67aeb5423d
Yaroslav <nepipkapipka8902@gmail.com>
2022-10-09 23:18:21 +0300

    created

---
+++ ExamPatterns/ExamPatterns/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string> // строки
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian"); // подключить русский язык
+
+	cout << "Старт программы" << endl;
+	
+	system("pause");
+	return 0;
+}
